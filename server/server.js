9bec8d94bec18d74cb063698c7352a4254ae0cc7
Watchitgo333 <bruskyeric@gmail.com>
2023-01-30 21:22:24 -0600

    added auth, hubs, aps and routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-require('./config/config');
+// require('./config/config');
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const session = require('cookie-session');
 const { PORT, SERVER_SESSION_SECRET } = require('./config/config')
-console.log(SERVER_SESSION_SECRET)
-app.use(session({ secret: SERVER_SESSION_SECRET, maxAge:24*60*60*1000}));
 app.use(cors());
+app.use(session({ secret: SERVER_SESSION_SECRET, maxAge:24*60*60*1000}));
+app.use(require('./routes/auth'));
+app.use(require('./routes/hubs'));
 app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
